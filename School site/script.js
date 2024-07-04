010cd14d59f985bdf719e903f6921bef0e9665cb
Gurkamal Singh Jandu <134562899+JanduGurkamal@users.noreply.github.com>
2024-07-04 15:01:08 -0700

    Made all nerccassry changes to bring website to life

--- School site/script.js
+++ School site/script.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {myFunction()};
 
-var topp=document.getElementById("topp");
+var topp = document.getElementById("topp");
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -12,52 +12,20 @@ function myFunction() {
   }
 }
 
-
-
 document.getElementById("aaa1").addEventListener("mouseover", function() {
-  document.getElementById("activity").style.background = "url('./activity/1.webp')"
-  document.getElementById("mm").style.color = "white";
-  document.getElementById("mm2").style.color = "white";
-});
-  
-document.getElementById("aaa1").addEventListener("mouseout", function() {
-  document.getElementById("activity").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./activity/main.jpg')";
+  document.getElementById("activity").style.background = "url('./activity/1.webp')";
 });
 
 
 document.getElementById("aaa2").addEventListener("mouseover", function() {
-  document.getElementById("activity").style.background = "url('./activity/2.webp')"
-  document.getElementById("mm").style.color = "white";
-  document.getElementById("mm2").style.color = "white";
+  document.getElementById("activity").style.background = "url('./activity/2.webp')";
 });
-  
-document.getElementById("aaa2").addEventListener("mouseout", function() {
-  document.getElementById("activity").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./activity/main.jpg')";
-});
-
-
 
 
 document.getElementById("aaa3").addEventListener("mouseover", function() {
-  document.getElementById("activity").style.background = "url('./activity/3.jpg')"
-  document.getElementById("mm").style.color = "black";
-  document.getElementById("mm2").style.color = "black";
+  document.getElementById("activity").style.background = "url('./activity/3.jpg')";
 });
-  
-document.getElementById("aaa3").addEventListener("mouseout", function() {
-  document.getElementById("mm").style.color = "white";
-  document.getElementById("mm2").style.color = "white";
-  document.getElementById("activity").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./activity/main.jpg')";
-});
-
-
-
 
 document.getElementById("aaa4").addEventListener("mouseover", function() {
-  document.getElementById("activity").style.background = "url('./activity/4.jpg')"
+  document.getElementById("activity").style.background = "url('./activity/4.jpg')";
 });
-  
-document.getElementById("aaa4").addEventListener("mouseout", function() {
-  document.getElementById("activity").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./activity/main.jpg')";
-});
-
